a6f52dd99641580d428dba30455f50840bdcbdc1
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-08-05 21:00:37 +0300

    Add function to fetch age estimate for provided name

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Error;
+use serde_derive::Deserialize;
+
+const API_URL: &str = "https://api.agify.io";
+
+#[derive(Debug, Deserialize)]
+pub struct AgeEstimation {
+    pub count: u32,
+    pub name: String,
+    pub age: u8
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub async fn estimate_age(name: &str) -> Result<AgeEstimation, Error> {
+    let url = format!("{}?name={}", API_URL, name);
+    let response = reqwest::get(url).await?;
+    if response.status().is_success() {
+        let age_estimation: AgeEstimation = response.json().await?;
+        Ok(age_estimation)
+    } else {
+        Err(anyhow::anyhow!("api request failed with status: {}", response.status()))
     }
 }
