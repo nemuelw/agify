d4ea92329cc7880d8f19699006e14a002e46fb83
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-08-06 06:35:28 +0300

    Improve error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::Error;
+use anyhow::{anyhow, Error};
 use serde_derive::Deserialize;
 
 const API_URL: &str = "https://api.agify.io";
@@ -7,16 +7,27 @@ const API_URL: &str = "https://api.agify.io";
 pub struct AgeEstimation {
     pub count: u32,
     pub name: String,
-    pub age: u8
+    pub age: Option<u8>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AgifyError {
+    pub error: String
 }
 
 pub async fn estimate_age(name: &str) -> Result<AgeEstimation, Error> {
     let url = format!("{}?name={}", API_URL, name);
-    let response = reqwest::get(url).await?;
+    let response = reqwest::get(url).await.map_err(|e| {
+        anyhow!("{}", e)
+    })?;
     if response.status().is_success() {
         let age_estimation: AgeEstimation = response.json().await?;
         Ok(age_estimation)
     } else {
-        Err(anyhow::anyhow!("api request failed with status: {}", response.status()))
+        let api_error: Result<AgifyError, _> = response.json::<AgifyError>().await;
+        match api_error {
+            Ok(api_error) => Err(anyhow!("{}", api_error.error)),
+            Err(e) => Err(anyhow!("{}", e))
+        }
     }
 }
